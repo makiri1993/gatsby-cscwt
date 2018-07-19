d76bb7a6ac9af3b07fc51a7f80ba72ef08e521f7
Martin Kireew <ma__kiri@ma-kiri-pro.local>
2018-07-19 19:52:53 +0200

    Adding styled-Comp and Standard-Colors

---
+++ StyledComponents.jsx
@@ -0,0 +1,17 @@
+import Styled from 'styled-components';
+
+/* RGB */
+const color1 = 'rgba(85, 179, 74, 1)';
+const color2 = 'rgba(72, 163, 217, 1)';
+const color3 = 'rgba(246, 211, 74, 1)';
+const color4 = 'rgba(39, 41, 50, 1)';
+const color5 = 'rgba(152, 38, 73, 1)';
+
+console.log("object")
+const Header = Styled.div`
+    width: 100px;
+    height: 100px;
+    background-color: blue;
+`;
+
+export default Header;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Navbar from '../components/Navbar'
+import Header from '../../StyledComponents'
 import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet title="Home | Gatsby + Netlify CMS" />
     <Navbar />
+    <Header />
     <div>{children()}</div>
   </div>
 )
